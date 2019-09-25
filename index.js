c4fa7faba2b400e9d05a12af7547d8c2fe15c129
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-25 07:19:58 +0200

    craiglist software jobs

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+
+
+async function main() {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof');
+    const html = await page.content();
+
+    const $ = cheerio.load(html);
+    $(".result-title").each((index, element) => console.log($(element).text()));
+
+    debugger;
+}
