96e9d50cba974195a0ad859c0ca46675d15b7546
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-09-25 07:55:15 +0200

    scraping list of pages

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
-// async function main() {
-(async() => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+async function scrapeListings(page) {
+    // (async() => {
     await page.goto('https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof');
     const html = await page.content();
 
     const $ = cheerio.load(html);
 
-    const results = $(".result-info").map((index, element) => {
+    const listings = $(".result-info").map((index, element) => {
         const titleElement = $(element).find('.result-title');
         const timeElement = $(element).find('.result-date');
         const hoodElement = $(element).find('.result-hood');
@@ -18,12 +16,30 @@ const cheerio = require('cheerio');
         const url = $(titleElement).attr('href');
         const datePosted = new Date($(timeElement).attr('datetime'));
         const hood = $(hoodElement).text().trim().replace('(', '').replace(')', '');
-
-
         return { title, url, datePosted, hood };
     }).get();
 
-    console.log(results);
-    debugger;
-})();
-// }
+    return listings;
+    // })();
+}
+
+async function scrapeJobDescriptions(listings, page) {
+    for (let i = 0; i < listings.length; i++) {
+        await page.goto(listings[i].url);
+        const html = await page.content();
+    }
+}
+
+
+async function main() {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    const listings = await scrapeListings(page);
+
+    const listingsWithJobDescriptions = await scrapeJobDescriptions(listings, page);
+
+    console.log(listings);
+}
+
+main();
+debugger;
